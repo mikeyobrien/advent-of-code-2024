3a4b2a29a8a67412d382240724794694302500e4
Mikey O'Brien <me@mikeyobrien.com>
2024-12-02 20:03:57 -0600

    day1-part2

--- day1/main.go
+++ day1/main.go
@@ -20,21 +20,26 @@ func main() {
 	lines := strings.Split(string(input), "\n")
 	list1 := []float64{}
 	list2 := []float64{}
+	appearanceCounts := map[float64]int{}
 	for _, line := range(lines[:len(lines)-1]) {
 		ids := strings.Split(line, "   ")
 		id1, _ := strconv.ParseFloat(ids[0], 64)
 		id2, _ := strconv.ParseFloat(ids[1], 64)
 		list1 = append(list1, id1)
 		list2 = append(list2, id2)
+		appearanceCounts[id2]++
 	}
 	sort.Float64s(list1)
 	sort.Float64s(list2)
 
 	res := 0
+	similarityScore := 0
 	for i, _ := range(list1) {
 		diff := math.Abs(list1[i] - list2[i])
-		fmt.Println(list1[i], list2[i], diff)
+		similarity := int(list1[i]) * appearanceCounts[list1[i]]
+		fmt.Println(list1[i], list2[i], diff, similarity)
 		res += int(diff)
+		similarityScore += similarity
 	}
-	print(res)
+	fmt.Println(res, similarityScore)
 }
